11700c980322bb5449e29597901dbd8c24e68690
yohans hailu <emailyyohans@gmail.com>
2024-03-07 20:10:36 +0300

    added cors

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 from bs4 import BeautifulSoup
 import os
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/")
 def ScrapeIt():
