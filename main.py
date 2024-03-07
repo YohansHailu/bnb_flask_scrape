15e9f2304ee139bf9dd5a668baa2bec494d11489
yohans hailu <emailyyohans@gmail.com>
2024-03-07 10:20:56 +0300

    final commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from bs4 import BeautifulSoup
+import requests
+
+app = Flask(__name__)
+@app.route("/")
+
+def ScrapeIt():
+
+    url = "https://coinmarketcap.com/currencies/bullperks/"
+    response = requests.get(url)
+    html_content = response.text
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    divs_with_circulating_supply = soup.find_all('div', string="Circulating supply")
+    grandparent = divs_with_circulating_supply[0].find_parent().find_parent()
+    circulating_supply = grandparent.find('dd').text
+
+    circulating_supply =  circulating_supply.replace(',', '').replace('BLP', '')
+
+
+    divs_with_total_supply = soup.find_all('div', string="Total supply")
+    grandparent = divs_with_total_supply[0].find_parent().find_parent()
+    total_supply = grandparent.find('dd').text
+    total_supply = total_supply.replace(',', '').replace('BLP', '')
+
+
+    return f"Circulating Supply: {circulating_supply}, Total Supply: {total_supply}"
+
+
+
